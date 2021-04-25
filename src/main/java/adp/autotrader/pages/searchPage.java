abfc2fe1b1cdb956ee4fcd80e73d93bc18fd27f8
lakkojuk <kedara.hiranmai.lakkoju@adp.com>
2021-04-25 17:22:50 -0400

    pushing the change after pulling the latest version

--- src/main/java/adp/autotrader/pages/searchPage.java
+++ src/main/java/adp/autotrader/pages/searchPage.java
@@ -1,5 +1,8 @@
-public class SearchPage {
-    public void SearchPage(){
-        System.out.println("add a new file from git to learn pull the latest code ");
+package adp.autotrader.pages;
+
+public class searchPage {
+
+    public void searchPage(){
+        System.out.println("update file after pulling the latest version  form git ");
     }
 }
